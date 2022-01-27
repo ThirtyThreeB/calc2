8534d58d01f1b64c456d10db4a9578715eef67d4
Gabriel Aragon <thirtythreebananas@gmail.com>
2022-01-27 12:11:34 -0800

    cleanup

--- lib/calc/operations.ex
+++ lib/calc/operations.ex
@@ -1,22 +1,19 @@
-defmodule Operations do
+defmodule Calc.Operations do
   def calculate(nil), do: nil
 
   def calculate(first, second, operator) do
-    result =
-      case operator do
-        "+" ->
-          String.to_integer(first) + String.to_integer(second)
+    case operator do
+      "+" ->
+        String.to_integer(first) + String.to_integer(second)
 
-        "-" ->
-          String.to_integer(first) - String.to_integer(second)
+      "-" ->
+        String.to_integer(first) - String.to_integer(second)
 
-        "*" ->
-          String.to_integer(first) * String.to_integer(second)
+      "*" ->
+        String.to_integer(first) * String.to_integer(second)
 
-        "/" ->
-          String.to_integer(first) / String.to_integer(second)
-      end
-
-    result
+      "/" ->
+        String.to_integer(first) / String.to_integer(second)
+    end
   end
 end
