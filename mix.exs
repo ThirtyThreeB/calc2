c85694d4d91ea840b92d835c417621da3c5e5d86
Gabriel Aragon <thirtythreebananas@gmail.com>
2022-01-31 10:07:05 -0800

    allow elixir 1.11

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Calc.MixProject do
     [
       app: :calc,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
